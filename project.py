bd88ae23b5ed3098e8fd0174549a8238e587d835
Kerim Nezo <kerim.nezo.20@size.ba>
2022-11-26 21:12:31 +0100

    Bazna klasa za pravljenje API poziva sa req

--- project.py
+++ project.py
@@ -1,1 +1,15 @@
 #Projekat koji se pravio na kraju videa "Python Tutorial for Begginers"
+import requests
+
+class APICall:
+    '''Base class to make basic API call with requests'''
+
+    def call(self, method, api_url): #method(get,post,put,delete) i endpoint (tj api)
+        if method.lower() == "get": #.lower() jer nismo sigurni hoce li biti upper ili lower
+            r = requests.get(api_url) #pravimo get request sa api-jem
+
+        match r.status_code:
+            case 200 | 201 | 202:
+                return r.json()["value"]
+            case _:
+                raise TypeError("API error")
